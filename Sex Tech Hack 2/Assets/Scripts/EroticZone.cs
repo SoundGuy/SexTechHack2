09114e6914655767c4ad2a09cb819892cc637df4
Oded Sharon <git@odedsharon.com>
2017-11-26 15:34:17 +0200

    adding sounds

---
+++ Sex Tech Hack 2/Assets/Scripts/EroticZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EroticZone : MonoBehaviour {
+
+    public AudioSource Mouth;
+    public SoundPlayer MouthSoundPlayers;
+    //public AudioClip[] audioClips;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        PlaySound();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+
+        //choose sound
+        Mouth.Play();
+    }
+}
+
+
