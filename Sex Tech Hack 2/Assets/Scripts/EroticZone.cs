3d7da34cdf21b2ce766fdf5207eca46ec148266a
Oded Sharon <git@odedsharon.com>
2017-11-26 15:42:54 +0200

    addded more randomality

--- Sex Tech Hack 2/Assets/Scripts/EroticZone.cs
+++ Sex Tech Hack 2/Assets/Scripts/EroticZone.cs
@@ -34,6 +34,8 @@ public class EroticZone : MonoBehaviour {
     {
 
         //choose sound
+        MouthSoundPlayers.chooseNext();
+
         Mouth.Play();
     }
 }
--- Sex Tech Hack 2/Assets/Scripts/SoundPlayer.cs
+++ Sex Tech Hack 2/Assets/Scripts/SoundPlayer.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour {
 
+    public AudioSource audioSource;
     public AudioClip[] audioClips;
+    public int currentClip;
     // Use this for initialization
     void Start () {
-		
-	}
+        currentClip = 0;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void chooseNext()
+    public void chooseNext()
     {
-
+        currentClip++;
+        if (currentClip >= audioClips.Length)
+            currentClip = 0;
+        audioSource.clip = audioClips[currentClip];
     }
 }
